a98fafa1309573c6635ef9704a19c4ec41e47ed3
Kevin Tan <astral.abyss7@gmail.com>
2025-10-21 21:01:29 -0700

    preplaytest values
    everything works 👍 just want feedback on speeds and FOVs. Goal for the speeds and FOV is to make it feel like the player is a kinda ghost, jumping from bird to bird half-possessing them

--- Assets/Scripts/TEST mouse ctrl cam.cs
+++ Assets/Scripts/TEST mouse ctrl cam.cs
@@ -1,45 +1,57 @@
 using UnityEngine;
 
 /* TODO:
--- shakes violently when too close
+-- test speeds
+-- get playtest feedback on the speeds
 */
 
 public class TESTmousectrlcam : MonoBehaviour
 {
-    [SerializeField] private float turnSpeed = 6f;
-    [SerializeField] private float moveSpeed = 10f;
+    [Header("Chase / Tracking")] 
+    [SerializeField] private float turnSpeed = 10f;
+    [SerializeField] private float moveSpeed = 20f;
+    [SerializeField] private float proximity = 2f;
+    [Header("FOV")]
+    [SerializeField] private float baseFOV = 60f;
+    [SerializeField] private float chaseFOV = 90f;
+
 
     Transform target;
 
     void Update()
     {
+        Debug.Log("FOV: " + Camera.main.fieldOfView);
+
+        // switch when LMB is pressed
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
-            {
                 target = hit.collider.gameObject.transform.parent.Find("Camera Position");
-            }
         }
 
         if (target)
         {
-            // Turn toward target
+            // move n turn toward target
             Vector3 dir = (target.position - transform.position).normalized;
-            if (dir != Vector3.zero || Vector3.Distance(transform.position, target.position) > 0.3f)
+            if (dir != Vector3.zero && Vector3.Distance(transform.position, target.position) > proximity)
             {
+                Camera.main.fieldOfView = chaseFOV;
+
                 Quaternion rot = Quaternion.LookRotation(dir);
                 transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * turnSpeed);
+
+                transform.position += transform.forward * moveSpeed * Time.deltaTime;
             }
+            // but if too close, just snap to a set position
             else
             {
-                transform.LookAt(target.transform.parent.Find("Camera Target"));
+                Camera.main.fieldOfView = baseFOV;
+                transform.position = target.position;
+                transform.rotation = target.rotation;
             }
-
-            // Move forward
-            transform.position += transform.forward * moveSpeed * Time.deltaTime;
         }
     }
 }
